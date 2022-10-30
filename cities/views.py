8957cf7d23a8da376677016eb89bd98424b91a31
Zafar Davlatov <97242244+talxa@users.noreply.github.com>
2022-10-31 03:50:49 +0500

    Update app-cities and migarte it

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import City
 
-# Create your views here.
+__all__ = (
+    'home',
+)
+def home(request):
+    qs = City.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'city_home.html', context)
