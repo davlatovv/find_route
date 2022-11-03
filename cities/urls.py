f008f3c185774f3e8b6e0e386ae6873f0d000436
Zafar Davlatov <97242244+talxa@users.noreply.github.com>
2022-11-03 12:57:19 +0500

    Add detail/ aand add/ urls

--- cities/urls.py
+++ cities/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('add/', CityCreateView.as_view(), name='create'),
 ]
